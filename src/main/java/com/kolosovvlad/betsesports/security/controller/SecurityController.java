7c76a3229e30db7435eb691dded3c344f42913a2
Kolosov Vlad <kolosov.vd.456@bk.ru>
2022-12-28 15:33:39 +0300

    Controller

--- src/main/java/com/kolosovvlad/betsesports/security/controller/SecurityController.java
+++ src/main/java/com/kolosovvlad/betsesports/security/controller/SecurityController.java
@@ -1,7 +1,13 @@
 package com.kolosovvlad.betsesports.security.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SecurityController {
+    @GetMapping("/")
+    public void securityPage(){
+        return;
+    }
+
 }
