742cbd451d60a4b877700586b62e5f84cb701d27
Kolosov Vlad <kolosov.vd.456@bk.ru>
2022-12-28 18:59:14 +0300

    Change controller Ann

--- src/main/java/com/kolosovvlad/betsesports/security/controller/SecurityController.java
+++ src/main/java/com/kolosovvlad/betsesports/security/controller/SecurityController.java
@@ -2,11 +2,12 @@ package com.kolosovvlad.betsesports.security.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 
 @Controller
 public class SecurityController {
     @GetMapping("/")
     public void securityPage() {
-        System.out.println("All work"); //from master
+        System.out.println("Delete all Prodject");
     }
 }
