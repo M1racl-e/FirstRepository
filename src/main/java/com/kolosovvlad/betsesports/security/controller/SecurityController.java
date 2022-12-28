8f0e45f4a125588e1f734797163207b99366fa44
Kolosov Vlad <kolosov.vd.456@bk.ru>
2022-12-28 20:11:10 +0300

    Add changes

--- src/main/java/com/kolosovvlad/betsesports/security/controller/SecurityController.java
+++ src/main/java/com/kolosovvlad/betsesports/security/controller/SecurityController.java
@@ -1,6 +1,7 @@
 package com.kolosovvlad.betsesports.security.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -8,5 +9,10 @@ public class SecurityController {
     @PostMapping("/")
     public void securityPage() {
         System.out.println("Send going");
+    }
+
+    @GetMapping("/api")
+    public void secr(){
+        System.out.println("Sect");
     }
 }
